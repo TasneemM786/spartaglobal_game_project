f9b0ffa891d2978f38680759d01495c2c1784379
Tasneem Mahmud <tmahmud@spartaglobal.com>
2018-08-13 16:24:14 +0100

    added a timer

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 $(function(){
-  alert()
+
+  // accessing elements on the page
+  var time = 10;
+  var timer = $('.timer');
+  // set the timer to be 10
+  $(timer).html(time);
+
+  // this will run every second forever
+  var explosionTimer = setInterval(countDown, 1000);
+
+  //  declaring function
+  function countDown() {
+    if (time > 0) {
+      $(timer).html(time - 1);
+      time -= 1;
+    }else {
+      // call the function to change the box to green
+    }
+  }
+
 })
