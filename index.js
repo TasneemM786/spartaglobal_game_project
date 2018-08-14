d1a2eb215650d4e338f41ab85aa8ad2a694e5f3c
Tasneem Mahmud <tmahmud@spartaglobal.com>
2018-08-14 10:44:09 +0100

    used jquery animate to move red boxes

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ $(function(){
 
   // accessing elements on the page
   var box = $('.redBox');
-  var time = 10;
+  var time = 3;
   var timer = $('.timer');
   // set the timer to be 10
   $(timer).html(time);
 
   // this will run every second forever
   var explosionTimer = setInterval(countDown, 1000);
+  var showRandomBow = setInterval(createBox, 1000);
+
+  // function to move redBox
+
 
   //  declaring function
   function countDown() {
@@ -18,7 +22,20 @@ $(function(){
     } else {
       // call the function to change the box to green
       $(".redBox").css("background-color", "green");
+      $(".redBox").css("position", "absolute")
+      $(".redBox").animate({
+        left: "+=80vw",
+        top: "+=40vh",
+        width: "5px",
+      }, 1500 );
+      // $(".redBox").stop();
+      clearInterval(explosionTimer);
+
     }
   }
 
+  // create function to create redBox
+    // use jquery to create a div
+    // display on the pay $(body).append("element")
+    
 })
