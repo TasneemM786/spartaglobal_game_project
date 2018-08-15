87f4079238133ba20e32c3bf3ccb8dcc15c6b513
Tasneem Mahmud <tmahmud@spartaglobal.com>
2018-08-15 22:08:34 +0100

    positioned boxes on html

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 $(function(){
-
   // accessing elements on the page
   var box = $('.redBox');
   var time = 3;
   var timer = $('.timer');
   // set the timer to be 10
   $(timer).html(time);
-
   // this will run every second forever
   var explosionTimer = setInterval(countDown, 1000);
-  var showRandomBow = setInterval(createBox, 500);
-
-  // function to move redBox
-
-
+  var showRandomBow = setInterval(createBox, 1000);
   //  declaring function
   function countDown() {
     if (time > 0) {
@@ -23,33 +17,40 @@ $(function(){
       // call the function to change the box to green
       $(".redBox").css("background-color", "green");
       $(".redBox").css("position", "absolute")
-      $(".redBox").animate({
-        left: "+=80vw",
-        top: "+=40vh",
-        width: "5px",
-      }, 1500 );
-      $(".redBox").stop();
+      // $(".redBox").animate({
+      //   left: "+=45vw",
+      //   top: "+=30vh",
+      //   width: "5px",
+      // }, 1500 );
+      // $(".redBox").stop();
       clearInterval(explosionTimer);
-
     }
   }
-
   // create function to create redBox
     // use jquery to create a div
     // display on the pay $(body).append("element")
-
+//
     function createBox() {
       $("body").append("<div class='redBox'></div>");
       $(".redBox").css("background-color", "green");
       $(".redBox").css("position", "absolute")
+      setInterval(function(){
+      }, 1000)
       $(".redBox").animate({
-        left: "+="+Math.random()*80+"vw",
-        top: "+="+Math.random()*80+"vh",
+        // generate random between 0-80
+        left: "+="+((Math.random()*60)-30)+"vw",
+        top: "+="+Math.random()*30+"vh",
         width: "5px",
-      }, 2000 );
-      // $(".redBox").stop();
+      }, 1500 );
+      var timeOut2 = setTimeout(function(){
+        //debugger;
+        $($(".redBox")[0]).attr('class', 'newClass');
+      }, 3000)
       clearInterval(explosionTimer);
+    }
 
+    function findCoordinates() {
+      return $($(".redBox")).position()
 
     }
 })
